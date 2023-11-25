cc100b2bdff158326c99d9b1c0c95e7d797d6de7
its-sky <smc9919@naver.com>
2023-11-26 04:21:27 +0900

    [Refactor] #9 - 촛불 조회 마지막 불 붙인 날짜 추가

--- src/main/java/org/sopt/server/dto/response/CandleResponseDto.java
+++ src/main/java/org/sopt/server/dto/response/CandleResponseDto.java
@@ -8,12 +8,13 @@ import org.sopt.server.domain.Feel;
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record CandleResponseDto(
         String date,
+        String lastFire,
         String tag,
         String title,
         Feel feel,
         String body
 ) {
     public static CandleResponseDto of(Candle candle, String tag) {
-        return new CandleResponseDto(candle.getDate().toString(), tag, candle.getTitle(), candle.getFeel(), candle.getBody());
+        return new CandleResponseDto(candle.getDate().toString(), candle.getDeadLine().minusYears(1).toString(), tag, candle.getTitle(), candle.getFeel(), candle.getBody());
     }
 }
