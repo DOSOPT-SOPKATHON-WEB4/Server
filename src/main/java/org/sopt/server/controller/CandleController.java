ceea1e550ca7d75986d94bc1834dc4b2734a999a
Choi YoungLyn <joeuni0129@gmail.com>
2023-11-26 02:28:08 +0900

    [Chore] #3 - 코드리뷰 반영

--- src/main/java/org/sopt/server/controller/CandleController.java
+++ src/main/java/org/sopt/server/controller/CandleController.java
@@ -5,10 +5,8 @@ import org.sopt.server.dto.request.CandleRequestDto;
 import org.sopt.server.global.response.ApiResponse;
 import org.sopt.server.global.response.SuccessType;
 import org.sopt.server.service.CandleService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/candle")
@@ -18,6 +16,7 @@ public class CandleController {
     private final CandleService candleService;
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public ApiResponse<?> createCandle(@RequestBody CandleRequestDto request) {
         candleService.createCandle(request);
         return ApiResponse.success(SuccessType.POST_CANDLE_SUCCESS);
